c53a690fc4421b868032d87187bb04ccd96447c7
Mineli <monte.nemi@gmail.com>
2019-12-12 15:09:34 +0100

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let jumbotron1 = document.querySelector("#jumbotron1");
+let jumbotron2 = document.querySelector("#jumbotron2");
+
+// setInterval(function() {
+//     jumbotron1.style.display = "none";
+//     jumbotron2.style.display = "none";
+// }, 3000)
