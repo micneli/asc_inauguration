99ce7f3f0908842e2201191cf171976967906931
Mineli <monte.nemi@gmail.com>
2019-12-16 10:11:05 +0100

    Added JS for all students

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+let students = [
+  {
+    name: "Anthony",
+    age: 38,
+    experience: "C++ programmation"
+  },
+
+  {
+    name: "Beatrice",
+    age: 52,
+    experience: "JavaScript programmation"
+  },
+
+  {
+    name: "Etienne",
+    age: 20,
+    experience: "Réseaux informatiques"
+  }
+];
+
+let name1 = document.getElementsByClassName("name1");
+let age1 = document.getElementsByClassName("age1");
+let experience1 = document.getElementsByClassName("experience1");
+
+let name2 = document.getElementsByClassName("name2");
+let age2 = document.getElementsByClassName("age2");
+let experience2 = document.getElementsByClassName("experience2");
+
 
 $('#myCarousel').carousel({
   interval: 5000
@@ -25,14 +53,32 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
 
     case 1:
         navbarText.innerHTML = "<h3>WANTED: ILIYAS, 22 ANS, WEB DEVELOPPEUR</h3>";
+        name1[0].textContent = students[0].name;
+        age1[0].textContent = students[0].age;
+        experience1[0].textContent = students[0].experience;
+        name2[0].textContent = students[0].name;
+        age2[0].textContent = students[0].age;
+        experience2[0].textContent = students[0].experience;
       break;
 
     case 2:
         navbarText.innerHTML = "<h3>WANTED: JOHANNA, 22 ANS, WEB DEVELOPPEUR </h3>";
+        name1[1].textContent = students[1].name;
+        age1[1].textContent = students[1].age;
+        experience1[1].textContent = students[1].experience;
+        name2[1].textContent = students[1].name;
+        age2[1].textContent = students[1].age;
+        experience2[1].textContent = students[1].experience;
       break;
 
     case 3:
-      navbarText.innerHTML = "<h3>WANTED: NEBO, 48 ANS, WEB DEVELOPPEUR </h3>";
+        navbarText.innerHTML = "<h3>WANTED: NEBO, 48 ANS, WEB DEVELOPPEUR </h3>";
+        name1[2].textContent = students[2].name;
+        age1[2].textContent = students[2].age;
+        experience1[2].textContent = students[2].experience;
+        name2[2].textContent = students[2].name;
+        age2[2].textContent = students[2].age;
+        experience2[2].textContent = students[2].experience;
     break;
 
     default:
