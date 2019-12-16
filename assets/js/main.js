63a008574ecb58d2f9d0272c970c9c8cd1504d20
Mineli <monte.nemi@gmail.com>
2019-12-16 11:35:08 +0100

    Ready to be finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,12 +47,11 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
 
   switch (id) {
     case 0:
-        // navbarTextStart.classList.add("#navbar-text-start");
-        navbarText.innerHTML = "<h3>WANTED: ACCESS CODE SCHOOL</h3>";
+        navbarText.innerHTML = "<h3>ACCESS CODE SCHOOL</h3>";
       break;
 
     case 1:
-        navbarText.innerHTML = "<h3>WANTED: ILIYAS, 22 ANS, WEB DEVELOPPEUR</h3>";
+        // navbarText.innerHTML = "<h3>WANTED: ANTHONY, 38 ANS, WEB DEVELOPPEUR</h3>";
         name1[0].textContent = students[0].name;
         age1[0].textContent = students[0].age;
         experience1[0].textContent = students[0].experience;
@@ -62,7 +61,7 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
       break;
 
     case 2:
-        navbarText.innerHTML = "<h3>WANTED: JOHANNA, 22 ANS, WEB DEVELOPPEUR </h3>";
+        // navbarText.innerHTML = "<h3>WANTED: BEATRICE, 53 ANS, WEB DEVELOPPEUR </h3>";
         name1[1].textContent = students[1].name;
         age1[1].textContent = students[1].age;
         experience1[1].textContent = students[1].experience;
@@ -72,43 +71,16 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
       break;
 
     case 3:
-        navbarText.innerHTML = "<h3>WANTED: NEBO, 48 ANS, WEB DEVELOPPEUR </h3>";
+        // navbarText.innerHTML = "<h3>WANTED: ETIENNE, 22 ANS, WEB DEVELOPPEUR </h3>";
         name1[2].textContent = students[2].name;
         age1[2].textContent = students[2].age;
         experience1[2].textContent = students[2].experience;
         name2[2].textContent = students[2].name;
         age2[2].textContent = students[2].age;
         experience2[2].textContent = students[2].experience;
-    break;
+      break;
 
     default:
         navbarText.textContent = "";
   }
 })
-
-//document.getElementById('carouselExample').addEventListener('slide.bs.carousel', function () {
-    // console.log('slide fired')
-// })
-
-// $('#carouselExample').on('slide.bs.carousel', function () {
-//     console.log('slide next clicked')
-// })
-
-/* <div id="carouselExample" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://cdn1.vectorstock.com/i/1000x1000/27/05/destination-flag-icon-flat-style-vector-19372705.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://cdn1.vectorstock.com/i/1000x1000/26/80/flag-icon-flat-style-vector-19372680.jpg" alt="Second slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div> */
