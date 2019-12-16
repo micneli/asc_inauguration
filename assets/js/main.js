92eb3b9649b2e73038fff8a1ec78f058fd9dd7ff
Mineli <monte.nemi@gmail.com>
2019-12-16 15:07:32 +0100

    Ready for final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,24 @@ let students = [
     name: "Anthony Montmiraill",
     age: 38,
     experience: "C++ programmation",
-    presentation: "Lorem ipsum 1"
-
+    presentation: "Lorem ipsum 1",
+    softSkills: ["Curiosité", "Adaptabilité", "Endurance", "Créativité", "Ninjutsu"]
   },
 
   {
     name: "Beatrice",
     age: 52,
     experience: "JavaScript programmation",
-    presentation: "Lorem ipsum 2"
+    presentation: "Lorem ipsum 2",
+    softSkills: ["Curiosité", "Adaptabilité", "Endurance", "Créativité", "Ninjutsu"]
   },
 
   {
     name: "Etienne",
     age: 20,
     experience: "Réseaux informatiques",
-    presentation: "Lorem ipsum 3"
+    presentation: "Lorem ipsum 3",
+    softSkills: ["Curiosité", "Adaptabilité", "Endurance", "Créativité", "Ninjutsu"]
   }
 ];
 
@@ -31,6 +33,12 @@ let age2 = document.getElementsByClassName("age2");
 let experience2 = document.getElementsByClassName("experience2");
 
 let presentation = document.getElementById("presentation");
+
+let skill1 = document.getElementsByClassName("skill1");
+let skill2 = document.getElementsByClassName("skill2");
+let skill3 = document.getElementsByClassName("skill3");
+let skill4 = document.getElementsByClassName("skill4");
+let skill5 = document.getElementsByClassName("skill5");
 
 
 $('#myCarousel').carousel({
@@ -61,6 +69,11 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
         name1[0].textContent = "Nom et prenom : " + students[0].name;
         age1[0].textContent = "Age : " + students[0].age;
         presentation.textContent = students[0].presentation;
+        skill1[0].textContent = students[0].softSkills[0];
+        skill2[0].textContent = students[0].softSkills[1];
+        skill3[0].textContent = students[0].softSkills[2];
+        skill4[0].textContent = students[0].softSkills[3];
+        skill5[0].textContent = students[0].softSkills[4];
         experience1[0].textContent = students[0].experience;
         name2[0].textContent = students[0].name;
         age2[0].textContent = students[0].age;
@@ -73,6 +86,11 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
         age1[1].textContent = students[1].age;
         experience1[1].textContent = students[1].experience;
         presentation.textContent = students[1].presentation;
+        skill1[1].textContent = students[1].softSkills[0];
+        skill2[1].textContent = students[1].softSkills[1];
+        skill3[1].textContent = students[1].softSkills[2];
+        skill4[1].textContent = students[1].softSkills[3];
+        skill5[1].textContent = students[1].softSkills[4];
         name2[1].textContent = students[1].name;
         age2[1].textContent = students[1].age;
         experience2[1].textContent = students[1].experience;
@@ -84,6 +102,11 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide (ev) {
         age1[2].textContent = students[2].age;
         experience1[2].textContent = students[2].experience;
         presentation.textContent = students[2].presentation;
+        skill1[2].textContent = students[2].softSkills[0];
+        skill2[2].textContent = students[2].softSkills[1];
+        skill3[2].textContent = students[2].softSkills[2];
+        skill4[2].textContent = students[2].softSkills[3];
+        skill5[2].textContent = students[2].softSkills[4];
         name2[2].textContent = students[2].name;
         age2[2].textContent = students[2].age;
         experience2[2].textContent = students[2].experience;
