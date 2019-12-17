14ff75dcd1554fc0d0062da8f9a8985696423a30
Mineli <monte.nemi@gmail.com>
2019-12-17 11:01:15 +0100

    Pages ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,125 +1,155 @@
 let students = [
   {
     name: "Anthony Montmirail",
+    surname: "Tony Montana(scarface)",
     age: 42,
     presentation: "Elle gère la fougère",
     passion: "Le domaine de la réparation m'a fait découvrir différents langages de programmation qui m’ont conduit à l’ACS. 'En mode Apprentissage'",
     softSkills: ["Présence", "Vision", "Audace", "Confiance", "Gestion du temps"],
-    imgPro: "images/pro/anthony-pro.png"
+    devise: "Demain nous appartient",
+    passionFun: "J'ai découvert la Trotinette et depuis je suis libéré, délivré ... lol"
   },
 
   {
     name: "Béatrice Hatsch",
+    surname: "Béa",
     age: 53,
     presentation: "Ne fait pas aux autres ce que tu n'aimerais pas qu’on te fasse",
     passion: "L’une de mes passions l’univers de  la création . J’aimerai devenir développeur d’application",
     softSkills: ["Le sens du collectif", "La présence", "La motivation", "La créativité", "NinLa confiancejutsu"],
-    imgPro: "images/pro/beatrice-pro.png"
+    devise: " ",
+    passionFun: " "
   },
 
   {
     name: "Etienne Schmitt",
+    surname: "Syrgo",
     age: 20,
-    presentation: "Lorem ipsum 3",
-    passion: "Réseaux informatiques",
-    softSkills: ["Curiosité", "Adaptabilité", "Endurance", "Créativité", "Ninjutsu"],
-    img: "images/pro/etienne-pro.png"
+    presentation: "Toujours conserver sa meilleure carte à porté de main",
+    passion: " ",
+    softSkills: ["Esprit critique", "Coopération", "Curiosité", "Créativité", "Ninjutsu"],
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Ilias Imouli",
+    surname: "Ili",
     age: 20,
     presentation: "Go hard or go home",
     passion: "Sport et voyage",
     softSkills: ["Autonome ", "Curieux ", "Créativite", "Sportif", "Organisé"],
-    img: "images/pro/ilias-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Isabelle Hennes",
+    surname: "Isa ou Zabou",
     age: 20,
     presentation: "#warrior : Toujours se relever, toujours recommencer, Interdit d’abandonner!",
     passion: "Passionnée par les arts-martiaux, la mode, le makeup et les réseaux sociaux!",
     softSkills: ["L’empathie", "Gestion du temps", "Créativite", "L'esprit d’entreprendre", "Curiosité"],
-    img: "images/pro/isabelle-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Jérôme Rinner",
+    surname: "Monsieur Fruit",
     age: 20,
     presentation: "La créativité est contagieuse, faites la tourner !",
     passion: "J'aime la simplicité, apprécier l'instant présent et m'émerveiller de la créativité de la vie, la nature",
     softSkills: ["Adaptabilité", "Esprit d'équipe", "Rigueur", "Passion", "Ecoute"],
-    img: "images/pro/jerome-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Joan Sor",
+    surname: "Jojo",
     age: 20,
     presentation: "Tout métier qui ne fait pas oublier le travail est un esclavage",
     passion: "while (travaille === plaisir et passion){console.log(“just do it”)};",
     softSkills: ["Motivation", "Créativité", "Ténacité", "Détermination", "Esprit d’équipe"],
-    img: "images/pro/joan-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Johanna Galli",
+    surname: "Djobi",
     age: 23,
     presentation: "Ce ne sont pas nos aptitudes qui montrent ce que nous sommes, ce sont nos choix",
     passion: "Les voyages, la musique, les montages vidéos, Harry Potter, les carnavals, et les amis",
     softSkills: ["Résolution des problèmes", "Gestion du temps", "Sens du collectif", "Créativité", "Empathie "],
-    img: "images/pro/johanna-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Julie Moulot",
+    surname: "Julie",
     age: 23,
     presentation: "Nous vivons tous dans le ventre d’un chien géant, tout le monde le sait mais personne ne dit rien du tout",
     passion: "Fan de Tagada Jones et Ultra Vomit",
     softSkills: ["Aime observer", "écouter", "trouver des solutions", "Créativité", "Empathie"],
-    img: "images/pro/julie-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Ludovic Marin",
+    surname: "Ludo",
     age: 40,
     presentation: "OVH - Serveur Dédié - Ubuntu 18.04 - PHP 7.2 phpMyAdmin - jQuery - Js Vanilla - Css3 - Bootstrap - DNS - HTTPS - référencement",
     passion: "J’aime la musique et tout ce qui sonne pop, soul et funky. Je suis également passionné du web auquel je contribu depuis 2005 à travers 2 plateformes que j’ai réalisé et que j’ai mis en ligne. L’une étant un média musical que j’ai depuis arrêté et l’autre une niche centrée autour du partage et de la vente d’instrumentales",
     softSkills: ["Curieux", "Astucieux", "Débrouillard", "Ponctuel", "Créatif"],
-    img: "images/pro/ludovic-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Maëra David",
+    surname: "Yaya ",
     age: 21,
     presentation: "Parmi les faibles, le plus fort est celui qui n’oublie pas sa faiblesse",
     passion: "Passionnée par la culture Japonaise, je suis également une grande amatrice de lecture en tout genre. J’aime beaucoup jouer aux jeux vidéos et dessiner. Niveau pro, je souhaiterai développer mes compétences afin de pouvoir travailler dans une entreprise ou une agence en tant que développeur web, et gagner en expérience pour grimper les échelons",
     softSkills: ["Empathie", "Créativité", "Motivation", "Communication", "Confiance"],
-    img: "images/pro/maera-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Messaouda Benchikh",
+    surname: "Messa le géant dormant",
     age: 37,
     presentation: "Nous avons tous des idées positives, c’est avec de l’énergie et de la créativité que nous pouvons les réaliser. Nous sommes présents pour trouver des solutions et pour vous faciliter la vie",
     passion: "Informatique et mathématique, histoire, nouvelles technologies, voyage et langues étrangères.",
     softSkills: ["L’innovation", "Travail en groupe", "Communication", "Motivation", "Productivité"],
-    img: "images/pro/messaouda-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Nebojsa Mihaljevic",
+    surname: "Nebo",
     age: 48,
     presentation: "Work hard and have fun!!",
     passion: "Sports, voyage, passer le temps avec la famille et les amis",
     softSkills: ["Travailleur", "Précis", "Fiable", "Sérieux", "Raisonnable"],
-    img: "images/pro/nebo-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Victor Lapostolle",
+    surname: "Raptaman",
     age: 25,
     presentation: "L’ignorant affirme, le savant doute, le sage réfléchit",
     passion: "Le web et les BD",
     softSkills: ["Curiosité", "Adaptabilité", "Endurance", "Créativité", "Patience"],
-    img: "images/pro/victor-pro.png"
+    devise: " ",
+    passionFun: " "
   },
   {
     name: "Thomas Loegell",
+    surname: "Jamy",
     age: 25,
     presentation: "Les rêves donnent l’envie d’être à demain",
     passion: "Passionné par les plantes dont je ne connais même pas les noms !",
     softSkills: ["Flexibilité", "Le sens du service", "L’esprit d’équipe", "La curiosité", "L’esprit d’entreprendre"],
-    img: "images/pro/thomas-pro.png"
+    devise: " ",
+    passionFun: " "
   }
 ];
 
@@ -128,9 +158,9 @@ let age1 = document.getElementsByClassName("age1");
 let passion1 = document.getElementsByClassName("passion1");
 let presentation1 = document.getElementsByClassName("presentation1");
 
-let name2 = document.getElementsByClassName("name2");
-let age2 = document.getElementsByClassName("age2");
-let passion2 = document.getElementsByClassName("passion2");
+let surname = document.getElementsByClassName("surname");
+let devise = document.getElementsByClassName("devise");
+let passionFun = document.getElementsByClassName("passionFun");
 
 let skill1 = document.getElementsByClassName("skill1");
 let skill2 = document.getElementsByClassName("skill2");
@@ -159,11 +189,11 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide(ev) {
 
   switch (id) {
     case 0:
-      navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>ACCESS CODE SCHOOL</span></h2>";
+      // navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>ACCESS CODE SCHOOL</span></h2>";
       break;
 
     case 1:
-      navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>ANTHONY</span></h2>";
+      // navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>ANTHONY</span></h2>";
       // name1[0].textContent = "Nom et prenom : " + students[0].name;
       age1[0].textContent = "Age : " + students[0].age;
       passion1[0].textContent = "Passion : " + students[0].passion;
@@ -173,14 +203,13 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide(ev) {
       skill3[0].textContent = students[0].softSkills[2];
       skill4[0].textContent = students[0].softSkills[3];
       skill5[0].textContent = students[0].softSkills[4];
-      passion2[0].textContent = students[0].experience;
-      name2[0].textContent = students[0].name;
-      age2[0].textContent = students[0].age;
-      passion2[0].textContent = students[0].passion;
+      surname[0].textContent = students[0].surname;
+      devise[0].textContent = students[0].devise;
+      passionFun[0].textContent = students[0].passionFun;
       break;
 
     case 2:
-      navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>BEATRICE</span></h2>";
+      // navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>BEATRICE</span></h2>";
       // name1[1].textContent = students[1].name;
       age1[1].textContent = students[1].age;
       passion1[1].textContent = students[1].passion;
@@ -190,13 +219,13 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide(ev) {
       skill3[1].textContent = students[1].softSkills[2];
       skill4[1].textContent = students[1].softSkills[3];
       skill5[1].textContent = students[1].softSkills[4];
-      name2[1].textContent = students[1].name;
-      age2[1].textContent = students[1].age;
-      passion2[1].textContent = students[1].passion;
+      surname[1].textContent = students[1].surname;
+      devise[1].textContent = students[1].devise;
+      passionFun[1].textContent = students[1].passionFun;
       break;
 
     case 3:
-      navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>ETIENNE</span></h2>";
+      // navbarText.innerHTML = "<h2><span style='color:red; font-weight: bold;'>ETIENNE</span></h2>";
       // name1[2].textContent = students[2].name;
       age1[2].textContent = students[2].age;
       passion1[2].textContent = students[2].passion;
@@ -206,9 +235,9 @@ $('#myCarousel').on('slide.bs.carousel', function onSlide(ev) {
       skill3[2].textContent = students[2].softSkills[2];
       skill4[2].textContent = students[2].softSkills[3];
       skill5[2].textContent = students[2].softSkills[4];
-      name2[2].textContent = students[2].name;
-      age2[2].textContent = students[2].age;
-      passion2[2].textContent = students[2].passion;
+      surname[2].textContent = students[2].surname;
+      devise[2].textContent = students[2].devise;
+      passionFun[2].textContent = students[2].passionFun;
       break;
 
     default:
